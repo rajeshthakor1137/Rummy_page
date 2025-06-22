b300224bdc4894c679db98af8a62e6d7500a043c
RAJ THAKOR <rajt52600@gmail.com>
2025-06-23 01:06:30 +0530

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Rummy Download Page",
+    "description": "Custom Rummy Game listing page with download buttons.",
+    "category": "Website",
+    "version": "1.0",
+    "depends": ["website"],
+    "data": [
+        "views/rummy_template.xml"
+    ],
+    "installable": True,
+    "application": True
+}
